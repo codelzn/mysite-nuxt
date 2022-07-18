a05758eef8481576d3ddd6ea05d37b92e9ac2c3d
codelzn <lzn724958732@gmail.com>
2022-07-18 15:01:56 +0900

    update:config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,30 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
+import PluginGLSL from 'vite-plugin-glsl';
+import PluginSVGLoader from 'vite-svg-loader';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  ssr: true,
+  target: 'static',
+  sourcemap: false,
+  app: {
+    head: {
+      htmlAttrs: { lang: 'ja' },
+      meta: [
+        { title: '私のブログ' },
+        { charset: 'utf-8' },
+        { lang: 'ja' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hider: 'description', name: 'description', content: '私のブログ' },
+      ],
+      link: [{ rel: 'icon', href: '/favicon.png' }],
+    },
+  },
+  vite: {
+    plugins: [PluginGLSL(), PluginSVGLoader({ svgo: false })],
+  },
+  modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    viewer: true,
+  }
+});
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="text-center text-7xl text-orange-400">僕のサイト</h1>
+  </div>
+</template>
+<script lang="ts" setup>
+</script>
