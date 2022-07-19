2dc0035e65454bcc9acd0c19746a754f2e665c3a
codelzn <lzn724958732@gmail.com>
2022-07-19 13:43:56 +0900

    update:background-shader

--- components/Background.vue
+++ components/Background.vue
@@ -3,40 +3,74 @@ import * as THREE from 'three'
 import fragmentShader from '~/assets/shaders/fragment.glsl'
 import vertexShader from '~/assets/shaders/vertex.glsl'
 
+const { gsap } = useGsap()
 const canvas = ref<HTMLCanvasElement>(null)
 class Sketch {
   public scene: THREE.Scene
   public camera: THREE.PerspectiveCamera
   public renderer: THREE.WebGLRenderer
-  public geometry: THREE.BoxGeometry
+  public geometry: THREE.SphereBufferGeometry
   public material: THREE.ShaderMaterial
   public mesh: THREE.Mesh
+  public clock: THREE.Clock
   constructor() {
     this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    this.camera.position.z = 5
+    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 10)
+    this.camera.position.z = 1
     this.renderer = new THREE.WebGLRenderer({
       canvas: canvas.value,
       antialias: true,
+      alpha: true,
+      precision: 'highp',
+      powerPreference: 'high-performance',
     })
     this.renderer.setSize(window.innerWidth, window.innerHeight)
-    this.geometry = new THREE.BoxGeometry(1, 1, 1)
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    this.clock = new THREE.Clock()
+    this.geometry = new THREE.SphereBufferGeometry(0.3, 32, 32)
     this.material = new THREE.ShaderMaterial({
+      side: THREE.DoubleSide,
+      wireframe: true,
       uniforms: {
-        time: { value: 0 },
+        uTime: { value: 0 },
+        customA: { value: 0 },
       },
       vertexShader,
       fragmentShader,
     })
     this.mesh = new THREE.Mesh(this.geometry, this.material)
     this.scene.add(this.mesh)
+    this.animate()
+    this.resize()
+  }
+  public animate() {
+    requestAnimationFrame(() => this.animate())
+    this.mesh!.rotation.x = this.clock.getElapsedTime()
+    this.mesh!.rotation.y = this.clock.getElapsedTime()
+    this.material.uniforms.uTime.value = this.clock.getElapsedTime()
     this.renderer.render(this.scene, this.camera)
+  }
+  public resize() {
+    this.resizeHandler()
+    window.addEventListener('resize', this.resizeHandler.bind(this))
+  }
+  private resizeHandler() {
+    this.camera.aspect = window.innerWidth / window.innerHeight
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
   }
 }
 onMounted(() => {
-  new Sketch()
+  const sketch = new Sketch()
+  gsap.to(sketch.material.uniforms.customA, {
+    duration: 5,
+    value: 0.4,
+    repeat: -1,
+    yoyo: true,
+  })
 })
 </script>
 <template>
-  <canvas class="fixed h-screen w-screen -z-10" ref="canvas" />
+  <canvas class="fixed -z-10 top-0 left-0" ref="canvas" />
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,26 @@
 <template>
   <div>
-    <h1 class="text-center text-7xl text-orange-400">僕のサイト</h1>
     <Background />
+    <h1 class="text-center text-7xl text-orange-400">僕のサイト</h1>
   </div>
 </template>
 <script lang="ts" setup>
 useHead({
-  title: '僕のサイト',
-})
-const { gsap } = useGsap()
+  title: "僕のサイト",
+});
+const { gsap } = useGsap();
 onMounted(() => {
-  gsap.fromTo('h1', {
-    opacity: 0,
-    y: -100,
-  }, {
-    opacity: 1,
-    y: 0,
-    duration: 1,
-  })
-})
+  gsap.fromTo(
+    "h1",
+    {
+      opacity: 0,
+      y: -100,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+    }
+  );
+});
 </script>
