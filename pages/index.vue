b027f7cdc7528f0d96ec8876c62daf863189997e
codelzn <lzn724958732@gmail.com>
2022-07-20 09:40:19 +0900

    fixbug:error-504

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,12 @@
     <Background />
     <h1 class="text-7xl text-orange-400 text-center">Code LZN</h1>
     <h2 class="text-5xl text-blue-500 text-center mt-3">サイト開発中です</h2>
+    <NuxtLink href="/gsapsketch">
+      <h3 class="text-center text-blue-400 text-4xl">GSAPSketch</h3>
+    </NuxtLink>
+    <NuxtLink href="/threesketch">
+      <h3 class="text-center text-blue-400 text-4xl">THREESketch</h3>
+    </NuxtLink>
   </div>
 </template>
 <script lang="ts" setup>
