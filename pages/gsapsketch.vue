d9040808d315833da8d75131d20e1f24c02c206b
codelzn <lzn724958732@gmail.com>
2022-07-20 09:55:36 +0900

    fixbug:gsap-selector

--- pages/gsapsketch.vue
+++ pages/gsapsketch.vue
@@ -2,20 +2,22 @@
   <div>
     <h1>Home Page</h1>
     <section class="s1">
-      <img id="img1" src="/smiles.png" alt="smiles" class="max-w-md" />
+      <img ref="img1" src="/smiles.png" alt="smiles" class="max-w-md" />
     </section>
     <section class="s2">
-      <img id="img2" src="/smiles.png" alt="smiles" class="max-w-md" />
+      <img ref="img2" src="/smiles.png" alt="smiles" class="max-w-md" />
     </section>
   </div>
 </template>
 <script lang="ts" setup>
+const img1 = ref<HTMLElement>(null);
+const img2 = ref<HTMLElement>(null);
 useHead({
   title: "Home Page",
 });
 const { gsap } = useGsap();
 const initGsap = () => {
-  gsap.to("#img1", {
+  gsap.to(img1.value, {
     duration: 1,
     delay: 0.5,
     x: 100,
